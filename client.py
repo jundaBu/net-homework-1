404925567bcba50662f0bbc45abe46f2a68e860d
Juan Buitrago <juandacarros@gmail.com>
2025-09-18 17:13:53 -0400

    Update client.py

--- client.py
+++ client.py
@@ -3,17 +3,17 @@ import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(("localhost", 12345))
 
-# Receive prompt for display name
+
 prompt = client_socket.recv(1024).decode()
 print(prompt, end="")
 display_name = input()
 client_socket.send(display_name.encode())
 
-# Receive welcome message
+
 welcome = client_socket.recv(1024).decode()
 print(welcome)
 
-# Send messages until "quit"
+
 while True:
     message = input("Enter a message (or 'quit' to exit): ")
     client_socket.send(message.encode())
